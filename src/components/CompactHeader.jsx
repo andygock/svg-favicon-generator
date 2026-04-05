e0cb3acdb28d34478f7340a945570fc609f5f5c6
Andy Gock <andy@gock.net>
2026-04-05 10:24:10 +1000

    feat: simplify CompactHeader layout and center title for improved aesthetics

--- src/components/CompactHeader.jsx
+++ src/components/CompactHeader.jsx
@@ -1,10 +1,7 @@
 export function CompactHeader({ status }) {
   return (
     <header className="app-header panel">
-      <div className="app-header__copy">
-        <span className="app-header__kicker">favicon generator</span>
-        <h1>SVG Favicon Generator</h1>
-      </div>
+      <h1>👉👉👉 SVG Favicon Generator 👈👈👈</h1>
     </header>
   );
 }
