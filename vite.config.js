3f0f5fa62aa3f54afe1f74c340bf76615be32a17
Andy Gock <andy@gock.net>
2026-04-05 11:36:11 +1000

    feat: migrate from React to Preact; remove React dependencies and update Vite configuration for Preact compatibility, bundle size reduced 75%

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: ".",
   base: "./",
-  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "react", replacement: "preact/compat" },
+      { find: "react-dom/test-utils", replacement: "preact/test-utils" },
+      { find: "react-dom", replacement: "preact/compat" },
+      { find: "react-dom/client", replacement: "preact/compat" },
+      { find: "react/jsx-runtime", replacement: "preact/jsx-runtime" },
+      { find: "react/jsx-dev-runtime", replacement: "preact/jsx-dev-runtime" },
+    ],
+  },
   build: {
     outDir: "dist",
   },
